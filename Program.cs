c4c0530eaeae1da72e05e942edeb5f8bca99d923
Antônio de Pádua <antonyejoao@gmail.com>
2024-05-03 13:28:48 -0300

    adicionando add, remove e list

--- Program.cs
+++ Program.cs
@@ -1,40 +1,86 @@
-﻿using NovoProjetoPOOO.models;
+﻿using System.Security.Cryptography.X509Certificates;
+using NovoProjetoPOOO.models;
 
 internal class Program
 {
+
     private static void Main(string[] args)
     {
 
-
-        Pessoa p1 = new Pessoa("Paulo", 12345678910);
-        p1.Apresentar();
-
+        
+        static void AgradecimentosPorUtilizarOPrograma()
+        {
+            Console.WriteLine("Obrigado por utilizar o programa!");
+        }
 
         string[] funcao = new string[] { "comprador", "vendedor", "intermediador" };
-
 
         Console.WriteLine("Bem vindo ao sistema de cadastro de comprador e intermediador! o que você é:\n Comprador = 1  Vendedor = 2 Intermediador = 3\n");
         int tipo = Convert.ToInt32(Console.ReadLine());
 
+
         Console.WriteLine("você quer \n1.adicionar\n2.remover\n3.listar\n4.editar");
         int menu = Convert.ToInt32(Console.ReadLine());
 
+
+        string Nome;
+        Pessoa pessoa = new Pessoa("antonio dev");
         switch (menu)
         {
             case 1:
-                Console.WriteLine("Adicionar " + funcao[tipo - 1]);
+                Console.WriteLine("Bem vindo ao adicionar" + funcao[tipo - 1] + "! digite o nome da pessoa:\n");
+                Nome = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(Nome))
+                {
+                    pessoa.AdicionarPessoa(Nome);
+                    Console.WriteLine("Pessoa cadastrada com sucesso!");
+                }
                 break;
 
             case 2:
-                Console.WriteLine("remover " + funcao[tipo -1]);
+                Console.WriteLine("Remover" + funcao[tipo - 1] + ":\n digite o nome da pessoa");
+                Nome = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(Nome))
+                {
+                    pessoa.RemoverPessoa(Nome);
+                    Console.WriteLine(funcao[tipo - 1] + " removida com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine(funcao[tipo - 1] + " não foi localizada.");
+                }
                 break;
 
             case 3:
-                Console.WriteLine("listar " + funcao[tipo -1]);
+                Console.WriteLine("Bem vindo a lista de cadastrados!");
+                pessoa.ListarPessoa();
+
                 break;
 
             case 4:
-                Console.WriteLine("editar " + funcao[tipo -1]);
+                Console.WriteLine("editar " + funcao[tipo - 1]);
+                Console.WriteLine("digite o nome que deseja alterar:");
+                string nome = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(nome))
+                {
+                    Console.WriteLine("Digite o novo nome da pessoa:");
+                    string novoNome = Console.ReadLine();
+
+                    Pessoa pessoaParaEditar = Pessoa.ListaDePessoas.Find(p => p.Nome == nome);
+                    if (pessoaParaEditar != null)
+                    {
+                        pessoaParaEditar.Nome = novoNome;
+                        Console.WriteLine("Pessoa editada com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pessoa não encontrada na lista.");
+                    }
+
+                }
                 break;
 
             case 5:
@@ -44,20 +90,18 @@ internal class Program
                 break;
 
             default:
+                Console.WriteLine("Comando inválido");
                 AgradecimentosPorUtilizarOPrograma();
                 Environment.Exit(0);
                 break;
         }
 
-        
+
     }
 
 
     //finalizando o programa e agradecendo o user
-    public static void AgradecimentosPorUtilizarOPrograma()
-    {
-        Console.WriteLine("Obrigado por utilizar o programa!");
-    }
+
 
     //isso é uma tupla
     //     (int, string, string) tupla = (21,"Paulo", "Paulo@PAULO.com");
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -8,33 +8,80 @@ using Microsoft.VisualBasic;
 
 namespace NovoProjetoPOOO.models
 {
+
+    //List<string> AdicaoDePessoa = new List<string>();
     public class Pessoa
     {
 
-        public string Nome;
-        public long Cpf;
-        public Pessoa(string nome, long cpf)
+        public Pessoa(string nome)
         {
             Nome = nome;
-            Cpf = cpf;
         }
+        public string Nome { get; set; }
+
+        string[] funcao = new string[] { "comprador", "vendedor", "intermediador" };
+        public List<Pessoa> ListaDePessoa = new List<Pessoa>();
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá! me chamo {Nome} e meu cpf é {Cpf}.");
-        }
-
-        public void AdicionarPessoa()
-        {
 
         }
 
-        public void RemoverPessoa()
+        public void AdicionarPessoa(string Nome)
         {
+            if (Nome != null)
+            {
+                Pessoa pessoa = new Pessoa(Nome);
+                Console.WriteLine($"o nome" + Nome + " foi cadastrado com sucesso");
+                ListaDePessoa.Add(pessoa);
+            }
+            else
+            {
+                Console.WriteLine("o nome não pode ser nulo");
+            }
+        }
 
+        public bool RemoverPessoa(string Nome)
+        {
+            if (Nome != null)
+            {
+                Pessoa pessoaParaRemover = ListaDePessoa.Find(p=>p.Nome == Nome);
+            
+                if (pessoaParaRemover != null)
+                {
+                    ListaDePessoa.Remove(pessoaParaRemover);
+                    Console.WriteLine($"O nome {Nome} foi removido com sucesso");
+                    return true;
+                }
+
+                else
+                {
+                    Console.WriteLine($"O nome {Nome} não foi encontrado na lista");
+                    return false;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("o nome não pode ser nulo");
+                return false;
+            }
+            
+            
         }
 
         public void ListarPessoa()
+        {
+            Console.WriteLine("Aqui está a lista de pessoas cadastradas:\n");
+            string Nome = Console.ReadLine();
+            Console.WriteLine($"o nome" + Nome + " foi cadastrado com sucesso");
+
+            foreach (var pessoa in ListaDePessoa)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+
+        public void EditarPessoa()
         {
 
         }
